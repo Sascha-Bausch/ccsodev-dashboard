0605e5de9b8e5145f55d128a0e6dfabfa2f2bbcf
SVA\sbausch <sascha.bausch@sva.de>
2022-03-09 16:57:39 +0100

    backend

---
+++ dashboard-client/src/services/Api.js
@@ -0,0 +1,8 @@
+// connector to backend for hitting different endpoints
+import axios from 'axios'
+
+export default () => {
+    return axios.create({
+        baseURL: `http://localhost:8080/`
+    })
+}
