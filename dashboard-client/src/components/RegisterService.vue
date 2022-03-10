cee327a108e6c787bfdab602f710442f17efef0e
SVA\sbausch <sascha.bausch@sva.de>
2022-03-10 15:45:26 +0100

    register2

--- dashboard-client/src/components/RegisterService.vue
+++ dashboard-client/src/components/RegisterService.vue
@@ -4,23 +4,50 @@
         <input 
             type="email"
             name="email"
+            v-model="email"
             placeholder="email" />
         <br>
         <input
             type="password"
             name="password"
+            v-model="password"
             placeholder="password" />
+        <br>
+        <button @click="register">Register</button>
     </div>
 </template>
 
 <script>
 export default {
     data () {
-
+        return {
+            email:'abc',
+            password: '123'
+        }        
+    },
+    watch: {
+        email (value) {
+            console.log('email has changed', value);
+        }
+    },
+    methods: {
+        register () {
+            console.log('button was clicked');
+        }
+    },
+    mounted () {
+        setTimeout(() => {
+            this.email = 'things done changed'
+        }, 2000)
     }
 }
 </script>
 
 <style scoped>
-
+input {
+    margin: 5px;
+}
+button {
+    margin: 5px;
+}
 </style>
