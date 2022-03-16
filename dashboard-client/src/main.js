91ccf77e532580c8f19cd577490caabe6ad61f08
SVA\sbausch <sascha.bausch@sva.de>
2022-03-16 10:17:03 +0100

    frontend backend no vuetify

--- dashboard-client/src/main.js
+++ dashboard-client/src/main.js
@@ -2,7 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-/* import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css'; */
 
 createApp(App).use(store).use(router).mount("#app");
