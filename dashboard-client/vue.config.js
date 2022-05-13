4189ade5c336b951807173ca88ab4df52108b195
SVA\sbausch <sascha.bausch@sva.de>
2022-05-13 09:48:20 +0200

    13.5.

--- dashboard-client/vue.config.js
+++ dashboard-client/vue.config.js
@@ -1,4 +1,14 @@
+// CommonJS-Modul
+
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+
+// ES-Modul
+
+/* import { defineConfig } from "@vue/cli-service";
+export default defineConfig({
+  transpileDependencies: true,
+}); */
